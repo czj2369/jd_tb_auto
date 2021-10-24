d1712025aac5c9e55dcf9856ff63eca5adf3ee40
czj <624278325@qq.com>
2021-10-24 18:05:33 +0800

    [TB]完善功能以及优化逻辑

--- auto_20211111/tb/main.js
+++ auto_20211111/tb/main.js
@@ -3,20 +3,34 @@
  * 
  * Author: czj
  * Date: 2021/10/21 13:01:32
- * Versions: 1.0.0
+ * Versions: 1.2.0
  * Github: https://github.com/czj2369/jd_tb_auto
  */
 
 // 需要忽略的任务中包含的关键字
-var IGNORE_LIST = ['农场', '芭芭农场', '下单', '蚂蚁森林', '淘特', '小互动', '点淘', '充话费', '参与合伙', '喂小鸡'];
+var IGNORE_LIST = ['农场', '芭芭农场', '下单', '蚂蚁森林', '淘特', '点淘', '充话费', '参与合伙', '喂小鸡', '斗地主'];
 // 点击之后返回的任务
 const BACK_LIST = [];
 const GO_View = '去浏览';
 const GO_FINISH = '去完成';
 const GO_SEARCH = '去搜索';
 // 需要做的任务
-const FINISHED_TASK = ['任务完成', '全部完成啦', '喵糖已发放'];
+const FINISHED_TASK = ['任务完成', '全部完成啦', '喵糖已发放', '任务已完成'];
 const VIEW_MOST = '去逛逛';
+// 判定是否进入到喵糖总动员
+var isEnterTaskUI = false;
+// 判断停留时间
+var JUDGE_TIME = 0;
+// 定时器
+var interval;
+
+var options = ["做任务", "掷骰子"];
+var i = dialogs.select("请选择一个选项", options);
+if(i >= 0){
+    console.info("您选择的是" + options[i]);
+}else{
+    toast("您取消了选择");
+}
 
 init();
 
@@ -26,12 +40,47 @@ init();
 function init() {
     start();
 
-    while (true) {
+    // 子线程开启计时
+    threads.start(function () {
+        if (interval == null) {
+            // 开启计时器，进行卡顿计时
+            // 启动定时器前，将计数器归为0
+            JUDGE_TIME = 0;
+            log("开启定时器");
+            interval = setInterval(function () {
+                JUDGE_TIME = JUDGE_TIME + 1;
+            }, 1000);
+        }
+    });
 
-        enterActivity();
+    if (0 == i) {
+        while (true) {
 
-        viewTask();
+            recoverApp();
+    
+            enterActivity();
+    
+            viewTask();
+    
+            transitioPperation();
+        }
+    }else{
+        while(!enterMiaoTang() && !textContains("骰子").exists()) {
 
+        }
+        var textContent = textContains("骰子").findOnce().text();
+        var count = textContent.split("，")[1];
+        console.info("获取当前骰子数：", count);
+        while (count > 0) {
+            if (textContains("骰子").findOnce().click()) {
+                // 计时器重置
+                JUDGE_TIME = 0;
+                count = count - 1;
+                console.info("掷骰子,剩余次数：", count);
+                sleep(5000);
+            }
+        }
+        console.info("掷骰子任务结束");
     }
 }
 
@@ -41,8 +90,12 @@ function init() {
 function start() {
     auto.waitFor()
     var appName = "com.taobao.taobao";
-    launch(appName);
-    console.info("启动淘宝APP");
+    if (launch(appName)){
+        console.info("启动淘宝APP");
+    }else{
+        console.info("请手动启动淘宝APP")
+    }
+    
     console.show();
 }
 
@@ -53,11 +106,7 @@ function enterActivity() {
     const SIGN_IN = '签到';
     const GET_REWARD = '领取奖励';
     const CUT_TEN_BILION = '赚糖';
-    if (className('android.widget.FrameLayout').depth(17).indexInParent(14).clickable().find().length == 1) {
-        console.info("进入喵糖总动员")
-        var button = className('android.widget.FrameLayout').depth(17).indexInParent(14).clickable().find()[0].bounds();
-        click(button.centerX(), button.centerY());
-    }
+    enterMiaoTang();
 
     // 签到
     if (text(SIGN_IN).exists()) {
@@ -69,10 +118,41 @@ function enterActivity() {
         text(GET_REWARD).findOne().click();
     }
 
-    // 进入活动中心
+    // 进入做任务界面
     if (textContains(CUT_TEN_BILION).exists()) {
         var button = textContains(CUT_TEN_BILION).findOnce().bounds();
         click(button.centerX(), button.centerY());
+    }
+
+    if (isEnterTaskUI && !textContains(CUT_TEN_BILION).exists()) {
+        if (back()) {
+            isEnterTaskUI = false;
+        }
+        
+    }
+    return false;
+}
+
+/**
+ * 进入喵糖总动员
+ * @returns 
+ */
+function enterMiaoTang() {
+    if (className('android.widget.FrameLayout').depth(17).indexInParent(14).clickable().find().length == 1) {
+        console.info("进入喵糖总动员,稍等5s即刻开始任务");
+        var button = className('android.widget.FrameLayout').depth(17).indexInParent(14).clickable().find()[0].bounds();
+        click(button.centerX(), button.centerY());
+        isEnterTaskUI = true;
+    }else {
+        if (desc("我的淘宝").exists() && desc("我的淘宝").findOne().click()) {
+            sleep(3000);
+            if (descContains("赢20亿红包").exists() && descContains("赢20亿红包").findOne().click()) {
+                console.info("进入喵糖总动员,稍等5s即刻开始任务");
+                isEnterTaskUI = true;
+                sleep(5000);
+                return true;
+            }
+        }
     }
 }
 
@@ -88,26 +168,37 @@ function viewTask() {
         for (index = 0; index < button.length; index++) {
             var buttonParent = button[index].parent();
             // 遍历'去完成'或者'去浏览'的父控件下面的子控件，判断是否存在IGNORE_LIST中包含的文字，如果存在，不执行该任务，否则执行
-            if (!recursionControl(buttonParent)) {
-                var isViewAndFollow = false;
-                // 判断是否直接返回
-                buttonParent.child(0).children().forEach(element => {
-                    print(element.text())
-                    for (i = 0; i < BACK_LIST.length; i++) {
-                        if (element.text().indexOf(BACK_LIST[i]) >= 0) {
-                            isViewAndFollow = true;
+            try {
+                if (!recursionControl(buttonParent)) {
+                    var isViewAndFollow = false;
+                    // 判断是否直接返回
+                    buttonParent.child(0).children().forEach(element => {
+                        print(element.text())
+                        for (i = 0; i < BACK_LIST.length; i++) {
+                            if (element.text().indexOf(BACK_LIST[i]) >= 0) {
+                                isViewAndFollow = true;
+                            }
                         }
+                    });
+                    
+                    log("正在进行任务:" + buttonParent.child(0).children()[0].text());
+
+                    if (isViewAndFollow) {
+                        viewAndFollow();
                     }
-                });
-
-                log("正在进行任务:" + buttonParent.child(0).children()[0].text());
-                if (isViewAndFollow) {
-                    viewAndFollow();
+    
+                    button[index].click();
+                    sleep(4000);
+                    if (id("titlebar").exists()) {
+                        huDong();
+                    }
+                    // 计时器重置
+                    JUDGE_TIME = 0;
+                    sleep(500)
+                    break;
                 }
-
-                button[index].click();
-                sleep(500)
-                break;
+            } catch (error) {
+                console.error(error);
             }
         }
     }
@@ -116,6 +207,8 @@ function viewTask() {
     if (text(GO_View).exists()) {
         sleep(500);
         text(GO_View).findOne().click();
+        // 计时器重置
+        JUDGE_TIME = 0;
         log("正在进行任务:" + text(GO_View).findOne().parent().child(0).children()[0].text());
         sleep(2000);
     }
@@ -124,6 +217,8 @@ function viewTask() {
     if (text(GO_SEARCH).exists()) {
         sleep(500);
         text(GO_SEARCH).findOne().click();
+        // 计时器重置
+        JUDGE_TIME = 0;
         log("正在进行任务:" + text(GO_SEARCH).findOne().parent().child(0).children()[0].text());
         sleep(2000);
     }
@@ -131,10 +226,19 @@ function viewTask() {
     // 去逛逛
     if (textContains(VIEW_MOST).exists()) {
         textContains(VIEW_MOST).findOnce().click();
+        // 计时器重置
+        JUDGE_TIME = 0;
         log("正在进行任务:" + textContains(VIEW_MOST).findOnce().parent().child(0).children()[0].text());
         sleep(2000);
     }
 
+    // 天天去领钱
+    if (text("打开链接").exists()) {
+        text("打开链接").findOne().click();
+        // 计时器重置
+        JUDGE_TIME = 0;
+        sleep(2000);
+    }
     isFinshed(FINISHED_TASK);
 
 }
@@ -142,6 +246,31 @@ function viewTask() {
 function customBack() {
     while (back()) {
         break;
+    }
+}
+
+/**
+ * 互动任务
+ */
+function huDong() {
+    if(id("titlebar").exists()) {
+        console.info("进入互动任务");
+        var titleBarXY = id("titlebar").findOne().bounds();
+        var x = titleBarXY.centerX();
+        var y = titleBarXY.centerY();
+
+        while (!textContains("执行").exists() && click(x, y)) {
+            console.info("未点击到任务，重试");
+            y = y+50;
+        }
+
+        y = titleBarXY.centerY();
+
+        while (textContains("喵糖+1").exists() && textContains("执行").exists() && click(x, y)) {
+            console.info("点击屏幕");
+            y = y+50;
+        }
+
     }
 }
 
@@ -184,6 +313,32 @@ function viewAndFollow() {
     sleep(1000);
 }
 
+/**
+ * 自动判断程序是否卡顿，恢复方法
+ * 判断依据：1.不在活动界面 2.停留某个界面长达30s
+ * @returns 
+ */
+function recoverApp() {
+    if (!text("查看红包").exists() && JUDGE_TIME > 45) {
+        if (back()) {
+            // 计时器重置
+            JUDGE_TIME = 0;
+            console.warn("停留某个页面超过45s,自动返回，重置定时器。");
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+/**
+ * 过渡操作
+ */
+function transitioPperation() {
+    if (text("我知道了").exists()) {
+        text("我知道了").click();
+    }
+}
 /**
  * Author: czj
  * Date: 2021/10/21 13:01:32
